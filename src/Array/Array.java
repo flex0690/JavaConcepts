7ae9b15152725dfa79d46f966780a5fb344151c2
tarun.kumar <tarun.kumar1@telekom-digital.com>
2026-01-07 16:46:05 +0530

    Array - Largest Element

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -1,4 +1,24 @@
 package Array;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Array {
+
+    void maxElementInArray() {
+        List<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(8);
+        arr.add(4);
+
+        System.out.println(Collections.max(arr));
+    }
+
+
+
+    public void main(String[] args) {
+        maxElementInArray();
+    }
 }
