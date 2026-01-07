950f1d2cdbdd36533be8cc4a72a53369c0d90f2d
tarun.kumar <tarun.kumar1@telekom-digital.com>
2026-01-07 16:52:08 +0530

    Array - Second Largest Elemenet

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -16,9 +16,27 @@ public class Array {
         System.out.println(Collections.max(arr));
     }
 
+    void SecondLargestElementInArray() {
+        List<Integer> arr = new ArrayList<>(List.of(4, 2, 32, 4, 4, 2, 2, 4));
+
+        int largest = arr.get(0);
+        int secondLargest = arr.get(0);
+
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr.get(i)>largest) {
+                secondLargest = largest;
+                largest = arr.get(i);
+            }
+        }
+
+        System.out.println(secondLargest);
+    }
+
 
 
     public void main(String[] args) {
-        maxElementInArray();
+//        maxElementInArray();
+
+        SecondLargestElementInArray();
     }
 }
