514fde748c09abbd9d2366b2174870e97f0415d6
tarun.kumar <tarun.kumar1@telekom-digital.com>
2026-01-13 17:20:02 +0530

    Array - reverseArray - removeDuplicatesFromArray - moveAllZeroesToEnd - rotateArrayByDPlace

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -1,7 +1,9 @@
 package Array;
 
+import java.text.CollationElementIterator;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Array {
@@ -32,11 +34,100 @@ public class Array {
         System.out.println(secondLargest);
     }
 
+    void reverseArray() {
+        List<Integer> arr = new ArrayList<>(List.of(3,4,5,3,8,2,5,5,4,10));
+
+        for(int i =0; i<arr.size()/2; i++){
+            int item1 = arr.get(i);
+            int item2 = arr.get(arr.size()-i-1);
+
+            arr.set(i,item2);
+            arr.set(arr.size()-i-1,item1);
+        }
+        System.out.println(arr);
+    }
+
+    void removeDuplicatesFromArray() { // for sorted array only
+        List<Integer> arr = new ArrayList<>(List.of(1,1,1,1,2,2,2,2,2));
+
+        int res = 0;
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr.get(i)!=arr.get(res)) {
+                res++;
+                arr.set(res,arr.get(i));
+            }
+        }
+
+        for (int i = 0; i <=res; i++) {
+            System.out.println(arr.get(i));
+        }
+    }
+
+    void moveAllZeroesToEnd() {
+        List<Integer> arr = new ArrayList<>(List.of(28,5,0,1,9,2,0,8));
+        int l = 0;
+        int r = l+1;
+
+        while (r < arr.size()) {
+
+           if (arr.get(l) == 0) {
+               if (arr.get(l)!= arr.get(r)) {
+                   Collections.swap(arr, l, r);
+                   l++;
+                   r++;
+               } else {
+                   r++;
+               }
+           } else {
+               r++;
+               l++;
+           }
+        }
+
+        System.out.println(arr);
+    }
+
+    List<Integer> swap(int i, int j, List<Integer> arr) {
+
+        while(i<j) {
+            int temp = arr.get(i);
+            arr.set(i,arr.get(j));
+            arr.set(j,temp);
+            i++;
+            j--;
+        }
+         return arr;
+    }
+
+    void rotateArrayByDPlace() {
+        List<Integer> arr = new ArrayList<>(List.of(28,5,0,1,9,2,0,8));
+        int d = 10;
+        d = d%arr.size();
+        arr = swap(0,d-1,arr);
+        arr = swap(d,arr.size()-1,arr);
+
+        Collections.reverse(arr);
+
+        System.out.println(arr);
+
+
+
+
+    }
+
 
 
     public void main(String[] args) {
 //        maxElementInArray();
 
-        SecondLargestElementInArray();
+//        SecondLargestElementInArray();
+
+//        reverseArray();
+
+//        removeDuplicatesFromArray();
+
+//        moveAllZeroesToEnd();
+
+        rotateArrayByDPlace();
     }
 }
